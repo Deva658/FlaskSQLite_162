50c494aad98952b51797e6fe73c66a61b006bc76
Deva658 <devaaditya271@gmail.com>
2026-01-07 15:37:12 +0700

    p

--- app.py
+++ app.py
@@ -2,36 +2,37 @@ import sqlite3
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-DR_NAME = 'books.db'
 
-def connectdb():
-    conn = sqlite3.connect(DR_NAME)
+DB_NAME = "books.db"
+
+def connect_db():
+    conn = sqlite3.connect(DB_NAME)
     conn.row_factory = sqlite3.Row
     return conn
 
 def init_db():
-    conn = connectdb()
-    conn.execute('''CREATE TABLE IF NOT EXISTS books
-                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    judul VARCHAR(100) NOT NULL,
-                    penulis VARCHAR(100) NOT NULL,
-                 );''')
+    conn = connect_db()
+    conn.execute('''CREATE TABLE IF NOT EXISTS books (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        judul VARCHAR(100) NOT NULL,
+                        penulis VARCHAR(100) NOT NULL
+                    );''')
     conn.commit()
     conn.close()
 
 @app.route('/')
 def index():
-    conn = connectdb()
+    conn = connect_db()
     books = conn.execute('SELECT * FROM books').fetchall()
     conn.close()
     return render_template('index.html', books=books)
 
 @app.route('/add', methods=['GET', 'POST'])
-def add_book():
+def add():
     if request.method == 'POST':
         judul = request.form['judul']
         penulis = request.form['penulis']
-        conn = connectdb()
+        conn = connect_db()
         conn.execute('INSERT INTO books (judul, penulis) VALUES (?, ?)', (judul, penulis))
         conn.commit()
         conn.close()
@@ -40,11 +41,10 @@ def add_book():
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
-    conn = connectdb()
+    conn = connect_db()
     book = conn.execute('SELECT * FROM books WHERE id = ?', (id,)).fetchone()
     if not book:
-        conn.close()
-        return 'Buku tidak ditemukan', 404
+        return "Buku tidak ditemukan", 404
     
     if request.method == 'POST':
         judul = request.form['judul']
@@ -59,7 +59,7 @@ def edit(id):
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    conn = connectdb()
+    conn = connect_db()
     conn.execute('DELETE FROM books WHERE id = ?', (id,))
     conn.commit()
     conn.close()
